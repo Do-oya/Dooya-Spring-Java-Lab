b37f6195a36b216f2c9fdfc0cd2310285f7f2af8
Do-oya <qkrwjdgus1441@naver.com>
2025-02-22 17:47:06 +0900

    learning 테스트

---
+++ baeldung/src/test/java/learning/baeldung/BaeldungApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.baeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
