063a37b14d44ab30abba40e3c2a06d1a903851cd
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-04-04 21:37:32 +0900

    TDD 실습

--- oop-practice/src/test/java/org/example/PasswordValidatorTest.java
+++ oop-practice/src/test/java/org/example/PasswordValidatorTest.java
@@ -2,9 +2,10 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.concurrent.locks.StampedLock;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 /**
@@ -19,5 +20,14 @@ public class PasswordValidatorTest {
     void validatePasswordTest() {
         assertThatCode(() -> PasswordValidator.validate("serverwizard"))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자를 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aabbcce", "aabbccddeeffg"})
+    void validatePasswordTest2(String password) {
+        assertThatCode(() -> PasswordValidator.validate(password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
     }
 }
