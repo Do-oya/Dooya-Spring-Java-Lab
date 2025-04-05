e8b4b21ae30d08100c00686a957b9282067238ce
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-04-05 15:51:28 +0900

    객체지향 실습 - 학점 계산기

---
+++ oop-practice/src/main/java/org/example/_ch03_4/Course.java
@@ -0,0 +1,33 @@
+package org.example._ch03_4;
+
+public class Course {
+    private final String subject;
+    private final int credit;
+    private final String grade;
+
+    public Course(String subject, int credit, String grade) {
+        this.subject = subject;
+        this.credit = credit;
+        this.grade = grade;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public double multiplyCreditAndCourseGrade() {
+        return credit * getGradeToNumber();
+    }
+
+    public double getGradeToNumber() {
+        return switch (this.grade) {
+            case "A+" -> 4.5;
+            case "A" -> 4.0;
+            case "B+ " -> 3.5;
+            case "B" -> 3.0;
+            case "C+" -> 2.5;
+            case "C" -> 2.0;
+            default -> 0;
+        };
+    }
+}
---
+++ oop-practice/src/main/java/org/example/_ch03_4/GradeCalculator.java
@@ -0,0 +1,18 @@
+package org.example._ch03_4;
+
+import java.util.List;
+
+public class GradeCalculator {
+    private final Courses courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = new Courses(courses);
+    }
+
+    public double calculateGrade() {
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
+
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
+    }
+}
