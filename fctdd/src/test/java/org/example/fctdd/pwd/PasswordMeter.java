c2689749fc58a03b3283c11c164016093dbf701e
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-04-27 13:15:50 +0900

    리팩터링

--- fctdd/src/test/java/org/example/fctdd/pwd/PasswordMeter.java
+++ fctdd/src/test/java/org/example/fctdd/pwd/PasswordMeter.java
@@ -1,5 +1,8 @@
 package org.example.fctdd.pwd;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordMeter {
     public PasswordStrength meter(String password) {
         if (password == null) {
@@ -11,11 +14,16 @@ public class PasswordMeter {
         if (password.length() < 8) {
             return PasswordStrength.NORMAL;
         }
-        Pattern pattern = Pattern.compile("[0-9]");
-        Matcher matcher = pattern.matcher(password);
-        if (!matcher.find()) {
+        boolean containsDigit = containsDigit(password);
+        if (!containsDigit) {
             return PasswordStrength.NORMAL;
         }
         return PasswordStrength.STRONG;
+    }
+
+    private static boolean containsDigit(String password) {
+        Pattern pattern = Pattern.compile("[0-9]");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
     }
 }
