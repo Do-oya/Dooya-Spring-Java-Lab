2c13e51e32cc9aef88d13b9efd4d0e57d0d68f5e
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-04-27 12:07:26 +0900

    tdd 실습 프로젝트 생성

---
+++ fctdd/src/main/java/org/example/fctdd/FctddApplication.java
@@ -0,0 +1,13 @@
+package org.example.fctdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FctddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FctddApplication.class, args);
+    }
+
+}
---
+++ fctdd/src/test/java/org/example/fctdd/FctddApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fctdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FctddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
