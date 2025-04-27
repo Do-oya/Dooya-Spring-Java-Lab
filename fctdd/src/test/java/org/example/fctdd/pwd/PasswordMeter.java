85258293c3a97d12b9875281b0844809e69c9e0b
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-04-27 12:31:37 +0900

    패스워드가 null인 경우도 테스트가 통과하게 수정

--- fctdd/src/test/java/org/example/fctdd/pwd/PasswordMeter.java
+++ fctdd/src/test/java/org/example/fctdd/pwd/PasswordMeter.java
@@ -2,6 +2,9 @@ package org.example.fctdd.pwd;
 
 public class PasswordMeter {
     public PasswordStrength meter(String password) {
+        if (password == null) {
+            return PasswordStrength.INVALID;
+        }
         if (password.equals("abcdABCD123")) {
             return PasswordStrength.STRONG;
         }
