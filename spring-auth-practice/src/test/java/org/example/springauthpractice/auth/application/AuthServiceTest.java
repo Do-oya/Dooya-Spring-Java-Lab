8d7480b05f6b9dd8dadf72f278ca036d15497ef5
Park JeongHyun <qkrwjdgus1441@naver.com>
2025-05-02 10:13:42 +0900

    테스트 수정

--- spring-auth-practice/src/test/java/org/example/springauthpractice/auth/application/AuthServiceTest.java
+++ spring-auth-practice/src/test/java/org/example/springauthpractice/auth/application/AuthServiceTest.java
@@ -9,10 +9,13 @@ import org.example.springauthpractice.user.infrastructure.UserRepositoryImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.*;
@@ -20,36 +23,43 @@ import static org.mockito.BDDMockito.*;
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
 
+    @InjectMocks
+    private AuthServiceImpl authService;
+
     @Mock
-    private UserJpaRepository userJpaRepository;
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @Mock
     private JwtUtil jwtUtil;
 
     @DisplayName("로그인 성공 테스트")
     @Test
-    void loginTest() {
+    void loginTest_Success() {
         // given
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        User mockUser = User.builder()
+        User testUser = User.builder()
                 .id(1L)
                 .email("testEmail")
                 .name("testName")
                 .password(passwordEncoder.encode("testPassword"))
                 .role(Role.USER)
                 .build();
-        given(userJpaRepository.findByEmail(mockUser.getEmail())).willReturn(mockUser);
-
-        UserRepository userRepository = new UserRepositoryImpl(userJpaRepository);
-        AuthService authService = new AuthServiceImpl(userRepository, jwtUtil, passwordEncoder);
 
         LoginRequest request = new LoginRequest("testEmail", "testPassword");
+
+        given(userRepository.findByEmail(testUser.getEmail())).willReturn(Optional.of(testUser));
+        given(passwordEncoder.matches(request.password(), testUser.getPassword())).willReturn(true);
+        given(jwtUtil.createAccessToken(testUser.getId(), testUser.getEmail(), testUser.getRole())).willReturn("mocked-jwt-token");
 
         // when
         LoginResponse response = authService.login(request);
 
         // then
-        assertThat(mockUser.getEmail()).isEqualTo(response.email());
-        then(userJpaRepository).should(times(1)).findByEmail(mockUser.getEmail());
+        assertThat(testUser.getEmail()).isEqualTo(response.email());
+        then(userRepository).should(times(1)).findByEmail(testUser.getEmail());
+        then(passwordEncoder).should(times(1)).matches(request.password(), testUser.getPassword());
+        then(jwtUtil).should(times(1)).createAccessToken(testUser.getId(), testUser.getEmail(), testUser.getRole());
     }
 }
